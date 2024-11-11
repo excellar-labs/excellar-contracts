a55c4a9427d3b9fbf1f9f68ba71832d164029875
Plamen Hristov <phristov@blocksoft.ltd>
2024-11-11 23:42:13 +0200

    Xusd on evm as erc20 (#4)

--- evm/script/XUSD.s.sol
+++ evm/script/XUSD.s.sol
@@ -1,17 +1,37 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
 import {XUSD} from "../src/XUSD.sol";
+import {ERC1967Proxy} from "openzeppelin-contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 contract XUSDScript is Script {
-    XUSD public xusd;
-
     function setUp() public {}
 
     function run() public {
-        vm.startBroadcast();
+        // Get deployment private key from environment
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address deployerAddress = vm.addr(deployerPrivateKey);
 
-        xusd = new XUSD();
+        // Get network-specific parameters
+        address initialOwner = vm.envAddress("INITIAL_OWNER");
+
+        console.log("Deploying XUSD with parameters:");
+        console.log("Deployer:", deployerAddress);
+        console.log("Initial Owner:", initialOwner);
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Deploy implementation
+        XUSD implementation = new XUSD();
+        console.log("Implementation deployed at:", address(implementation));
+
+        // Encode initialization data
+        bytes memory initData = abi.encodeWithSelector(XUSD.initialize.selector, initialOwner);
+
+        // Deploy proxy
+        ERC1967Proxy proxy = new ERC1967Proxy(address(implementation), initData);
+        console.log("Proxy deployed at:", address(proxy));
 
         vm.stopBroadcast();
     }
--- evm/src/XUSD.sol
+++ evm/src/XUSD.sol
@@ -1,13 +1,59 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract XUSD {
-    uint256 public number;
+import {Initializable} from "openzeppelin-contracts-upgradeable/proxy/utils/Initializable.sol";
+import {ERC20Upgradeable} from "openzeppelin-contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import {OwnableUpgradeable} from "openzeppelin-contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {ERC20PermitUpgradeable} from
+    "openzeppelin-contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
+import {ERC20BurnableUpgradeable} from
+    "openzeppelin-contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import {UUPSUpgradeable} from "openzeppelin-contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {AccessControlUpgradeable} from "openzeppelin-contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract XUSD is
+    Initializable,
+    ERC20Upgradeable,
+    OwnableUpgradeable,
+    ERC20PermitUpgradeable,
+    ERC20BurnableUpgradeable,
+    UUPSUpgradeable,
+    AccessControlUpgradeable
+{
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
     }
 
-    function increment() public {
-        number++;
+    function initialize(address initialOwner) public initializer {
+        __Ownable_init(initialOwner);
+        __ERC20_init("XUSD Token", "XUSD");
+        __ERC20Permit_init("XUSD Token");
+        __ERC20Burnable_init();
+        __UUPSUpgradeable_init();
+        __AccessControl_init();
+
+        _grantRole(DEFAULT_ADMIN_ROLE, initialOwner);
+        _grantRole(MINTER_ROLE, initialOwner);
     }
+
+    function decimals() public pure override returns (uint8) {
+        return 6;
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+    function addMinter(address minter) public onlyOwner {
+        grantRole(MINTER_ROLE, minter);
+    }
+
+    function removeMinter(address minter) public onlyOwner {
+        revokeRole(MINTER_ROLE, minter);
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 }
